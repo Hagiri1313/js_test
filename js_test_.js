feefd955307999f2497e6f4bc5a771247466522c
Hagiri <19hagiri96@gmail.com>
2021-07-18 08:59:59 +0300

    dobavil dni

--- js_test_.js
+++ js_test_.js
@@ -1,3 +1,19 @@
-console.log('kenny creates')
+let day= 31
+if (day>=1 && <=10) {
+    console.log(pervaya decada);
+}else if (day>=11 && <=21) {
+    console.log(vtoraya decada);
+}else if (day>=22 && <=31) {
+    console.log(tretyya decada);
+}else{
+    console.log("false");
+}
+
+
+
+
+
+
+
 
 

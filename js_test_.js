0aba7995f82e8decfad9503739acc21446293b70
Hagiri <19hagiri96@gmail.com>
2021-07-18 15:12:53 +0300

    vtoroi variant

--- js_test_.js
+++ js_test_.js
@@ -1,44 +1,10 @@
-const day= 5;
-if (day >=1 && day <=10) {
-    console.log("pervaya decada");
-}else if (day >=11 && day <=21) {
-    console.log("vtoraya decada");
-}else if (day >=22 && day <=31) {
-    console.log("tretyya decada");
-}else{
-    console.log("intrersnyi kalendar'");
+const day= prompt("enter your day");
+if (day && day <= 10){
+    console.log("pd");
+}else if (day && day >= 11 && day <= 21) {
+    console.log ("fd");
+
+}else if (day && day >= 21 && day <= 31) {
+    console.log("td");
 }
-
-const month= 2;
-if (month <= 2 || month === 12) {
-    console.log("winter");
-}else if (month >= 3 && month <= 5){
-    console.log("spring");
-}else if (month >= 6 && month <=8) {
-    console.log("summer");
-}else if (month >=9 && month <=11) {
-    console.log("autumn");
-}
-
-const min = 17;
-if (min >= 0 && min <= 15.59) {
-    console.log ("first chvert'");
-}else if (min >= 16 && min <= 30.59){
-    console.log("second chvert'");
-}else if (min >= 31 && min <= 45.59) {
-    console.log ("third chvert'");
-}else if (min >= 46 && min <= 59.59){
-    console.log("fourth chvert'")
-}else {
-    console.log ("false");
-}
-
-const age= 83;
-if (age >= 0 && age <= 17){
-    console.log("nesovershennoletnyi");
-}else {
-    console.log("sovershennoletnyi");
-}
-
-
 

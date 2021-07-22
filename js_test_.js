331fa00df7bc8cdc0c8c2c15127d81db4ab03c41
Hagiri <19hagiri96@gmail.com>
2021-07-23 00:27:52 +0300

    podpravil dz

--- js_test_.js
+++ js_test_.js
@@ -1,25 +1,24 @@
- //найдите суму чисел от 1 до 100 исользуя фор вайл  ду вайл
+//найдите суму чисел от 1 до 100 исользуя фор вайл  ду вайл
+let i= 0
+ let sumf = 0;
+ for ( ; i <= 100; i++){
+  sumf += i;
 
-// let sumf = 0;
-// for (i = 1; i <= 100; i++){
-//  sumf += i;
-//
-// }
-// console.log(sumf)
+ }
+ console.log(sumf)
 
-// let sumw = 0;
-// let i = 0
-// while ( i < 100) {
-//  i++
-//  sumw += i
- // }
- //console.log(sumw)
+ let sumw = 0;
+ let n = 0
+ while ( n < 100) {
+  n++
+  sumw += n
+ }
+console.log(sumw)
 
-// let sumd = 0;
-// let i = 0;
- //do {
-  //   i++
-  //   sumd += i;
- //}while (i <100)
-// console.log(sumd)
-
+ let sumd = 0;
+ let c = 0;
+do {
+   c++
+   sumd += c;
+}while (c < 100);
+ console.log(sumd)

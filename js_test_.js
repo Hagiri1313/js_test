506ac8a13d4ade59cf679afb50501515e965d32e
Hagiri <19hagiri96@gmail.com>
2021-08-03 20:05:45 +0300

    ne sdelal vtoryy zadachy, a pervay cherez ochen' strannyi metod

--- js_test_.js
+++ js_test_.js
@@ -1,277 +1,155 @@
+// Неопознанные летающие объедки
+// Домашнее задание
 
+                                // Задача 1
+let obj1= {
+    a:1,
+    b:2,
+};
 
+let obj2= {
+    a:1,
+    b:2,
+};
+let obj3= {
+    a:8,
+    b:9,
+};
 
-
-// HOMEWORK MASSIV
-//
-// HOMEWORK MASSIV
-
-// ZADANIE 1;
-let arr123 = [1, 2, 3, 4, 5, 6, 7, 8]
-
-function showSepar(start, end) {
-    if (start < end) {
-        let result = arr123.slice(start, end);
-        console.log(result);
-    } else if (start > end) {
-        let arr321 = arr123.reverse();
-        let result2 = arr321.splice(end, start)
-        console.log(result2);
+let obj4= {
+    a:1,
+    b:2,
+    c:3,
+};
+let obj5= {
+    number:1,
+    numberok:2,
+};
+const isObj = (firstObj, secondObj) => {
+    let obj1Props = Object.getOwnPropertyNames(firstObj);
+    let obj2Props = Object.getOwnPropertyNames(secondObj);
+    // Что значат записи выше: по сути у объекта нету такого свойства как .length, однако это свойствао есть у массива.
+    //Данная запись преобразует ключи объекта в массив. Например для первого моего объекта массив будет ["a","b"]. Далее сравниваем длинну массива.
+    // способ рабочий действует на все мои объекты и выовдит всё как нужно
+    if (obj1Props.length !== obj2Props.length){
+        return false;
     }
-}
-showSepar(2, 6);
-
-
-// ZADACHA 2
-const num = "123456789123456789";
-
-const text = '554254254245';
-
-function spliceNum(tmp, index, symbol) {
-
-  let result = ""
-  for (let i = index; i < tmp.length; i++) {
-    if (tmp[i] % 2 !== 0 && i < tmp.length-1){
-      result += tmp[i] + symbol;
-    } else {
-      result += tmp[i];
-    }
-  }
-  console.log (result)
-}
-
-spliceNum('111111111',3,"O");
-
-// ZADANIE 3;
-const array1 = [1,2,3,4,5,6,7,8,9,10]
-const array2 = ["Roma", "Sasha", "Vlad", "Oleg"];
-const wrongLengthArr= [1,2,3]
-function changeHalfs (arra) {
-    let array3=[]
-        if (arra.length % 2 === 0 && arra.length / 2) {
-            array3 = arra.splice(arra.length / 2)
-            console.log(array3);
-            a4= array3.concat(arra);
-            console.log(a4)
-        } else if (arra.length % 2 !== 0){
-            console.log ("Нечетная длинна масива")
+    for (const key in firstObj) {
+        if (firstObj[key] !== secondObj[key]) {
+            return false;
         }
-}
-changeHalfs(array1)
-changeHalfs(array2)
-changeHalfs(wrongLengthArr)
-
-// Zadacha so zvezdochkoi
-
-const arr3 = ['a', 'b'];
-const arr4 = ['b', 'c'];
-const arr5 = ['b', 'e', 'c'];
-const arr6 = ['b', 'b', 'e'];
-const arr7 = ['b', 'c', 'e'];
-const arr8 = ['b', 'e', 'c'];
-
-const disting = (value, index, self) => {
-  return self.indexOf(value) === index;
-}
-
-const intersection = (...arrays) => {
-  if (arrays.length > 1) {
-    let gapa = arrays[0]
-    for (let i = 1; i < arrays.length; i++) {
-      gapa = gapa.concat(arrays[i]);
     }
-    console.log('result =>>> ', gapa);
-    const gapa2 = [];
-
-    gapa.forEach(element => {
-      let numberOfRepition = 0;
-      for(let j = 0; j < gapa.length; ++j ) {
-        if(element === gapa[j]) {
-          ++numberOfRepition;
-        }
-      }
-
-      if (numberOfRepition > 1) {
-        gapa2.push(element);
-      }
-    });
-
-    const resultArray = gapa2.filter(disting);
-    console.log('resultArray ===>>> ', resultArray);
-  }
+    return true;
 }
+console.log(isObj(obj1 , obj2));
 
-intersection(arr3, arr4, arr5);
+                                                    //Задача2
+
+function Farm(leaveplace, ate) {
+    this.leaveplace = leaveplace;
+    this.ate = ate;
+}
+let pig = new Farm ("sarai", "morkva");
+let horse = new Farm ("stoilo", "apple");
+let rabbit = new Farm ("sarai", "morkva");
+let unicorn = new Farm ("stiolo", "apple");
 
 
 
 
 
-// function one () {
-//     return 1;
+
+
+
+// let user = {
+// name: "Koly",
+//   surname: "jokl",
 // }
-// console.log(one())
-//
-//
-// function showUserName (name, familia){
-//     console.log("Privet , tu snova vihodish na svaz'"+name, "" +familia);
-//
+// user.name = "ebashu"
+// delete user.name
+// console.log(user);
+
+// let alpha ={
+//   name: "gosha",
 // }
-// showUserName("Kolya","Nosachenko");
-//
-// //function loco () {
-// //    return 1;
-// //}
-// //console.log(loco())
-//
-//
-//
-// let nameOfUser = "Nik";
-// let surname = " Nos"
-// function showPrivet () {
-//     const mas= "Privetyli "
-//     console.log( mas+ nameOfUser +  surname);
+// let beta ={}
+// function isEmpty(obj){
+//   for (let i in obj){
+// return false
+//   }
+// return true
 // }
-// showPrivet(nameOfUser,  surname)
 //
+// console.log(isEmpty(beta));
+
+// let obj1 = {
+//   name: "Kolya",
+//   age: 24,
+// }
 //
-// function showAllArguments () {
-// console.log (arguments)
-// }showAllArguments(1, 2, 3, 4, 5, 100)
+// Object.keys(obj1).forEach((key) => console.log(obj1[key]));
+// let fruit = "apple"
+// let bag ={
+//     [fruit]: 21
+// }
+// console.log(bag)
+
+
+
+
+// function makeUser(name, age) {
+//     return {
+//         name: name,
+//         age: age
 //
+//     };
+// }
 //
-// function showAllArguments (... hgfh) {
-//     console.log (hgfh)
-// }showAllArguments(1, 2, 3, 4, 5, 100);
+// let user = makeUser("John", 30);
+// console.log(user); // John
+
+
+// let perm1 = 12312
+// let  perm2= perm1
+// perm2= 123
+// console.log(perm2 === perm1)
 //
+// let perm3= {name: "jaja"};
+// let perm4 =perm3;
+// perm4.name = "atlz"
+// console.log(perm3.name);
+
+// let user = {
+//     name: "John"
+// };
 //
-//
-// //osnovnoe dz
-// function sayage (age) {
-//     if (age > 18) {
-//         return "ti uge vzrosliy" ;
-//     }else {
-//         return "net takogo zakona" ;
+// let admin = user;
+//  user= null
+// console.log(user);
+// console.log( admin);
+
+// let user = {
+//     name: "Pete",
+//     age: 28,
+//     sizes: {
+//         height: 168,
+//         width: 60
 //     }
 // }
-// console.log(sayage(0))
 //
-// /*
+// function cloneObject(obj) {
+//     let clone = {};
 //
-// function showDacade (day) {
-//     const i = day
-//     if (i <= 10) {
-//         return "PD";
-//     } else if (i >= 11 && i <= 21) {
-//         return "SD";
-//     } else if (i >= 22 && i <= 31) {
-//         return "TD";
-//     }
-// }
-// console.log(showDacade(28))
-// */
-//
-//
-//
-//
-//
-// // bonus
-//
-// /*
-//
-// function showUmnog (start, end) {
-//     let resoult = 2
-//     for (let i = start; i <= end; i++) {
-//         if (i % 2 === 0) {
-//             resoult *= i
+//     for (let key in obj) {
+//         if (typeof obj[key] == "object") {
+//             clone[key] = cloneObject(obj[key]);
+//         } else {
+//             clone[key] = obj[key];
 //         }
 //     }
-//     return resoult
+//
+//     return clone;
 // }
-// console.log(showUmnog(6,10))*/
 //
-//
-// // let arr = ["bibop", 1, 3, 6, 7, 1]
-// // let deleterd = arr.pop ()
-// // console.log(deleterd)
-// // let deleterd2 = arr.shift()
-// // console.log(deleterd2)
-//
-//
-// // let alfa = "1 2 3 4 5"
-// // let beta = alfa.split( " " );
-// // console.log(beta);
-//
-//
-// // let arr1= [1, 2, 3, 4, 5, 6, 7]
-// // // for (i = 0; i <= arr1.length; i++){
-// // //     console.log(arr1 [i] )
-// // // }
-// // //
-// // // console.log (arr1.reverse())
-// //
-// // let arr2 = arr1.join(" ");
-// // console.log (arr2)
-// //
-// // console.log (arr1.indexOf(9))
-// //
-// // console.log (arr1.slice (4, 6) );
-
-
-
-
-// let arr4 = [
-//     {
-//         name: "AAA1",
-//         location: "SSS1",
-//     },
-//     {
-//         name:"AAA13",
-//         location: "SSS13",
-//     },
-//     {
-//         name:"AAA12",
-//         location: "SSS12",
-//     },
-// ];
-// let finItem = arr4.find((item) => item.name === "AAA1");
-// console.log (finItem);
-
-
-/*const arr1 = [1, 2];
-const arr2 = [2, 3];
-const arr3 = [4, 5];
-const arr4 = [5, 6];
-
-// const arr3 = ['a', 'b'];
-// const arr4 = ['b', 'c'];
-// const arr5 = ['b', 'e', 'c'];
-// const arr6 = ['b', 'b', 'e'];
-// const arr7 = ['b', 'c', 'e'];
-// const arr8 = ['b', 'e', 'c'];
-
-const intersection = (...arrays) => {
-    console.log("arrays ===>>>>  ", arrays);
-    if (arrays.length > 1){
-        // const hahha1 = arrays[0];
-        // const hahha2 = arrays[1]
-        // const ahah = hahha1.concat( hahha2)
-        // console.log(ahah)
-        let gapa = []
-        for (let i = 0; i < arrays.length; i++) {
-            gapa = arrays[i].concat(arrays[i+1]);
-        }
-        console.log(gapa);
-    }
-}
-intersection(arr1, arr2,);*/
-
-// function sayHello (...name123){
-//     console.log("Hello "+ name123[0] +  name123[1]);
-// }
-// const sdsdsss="Kolya";
-// sayHello(sdsdsss, " ghghg", "dsfsdfsd", "dfdsfsdf");
-
-
-
-
+// console.log(user);
+// console.log( cloneObject(user) );
